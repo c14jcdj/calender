5d22b420ddff09a029fb1b1f48b8619fbf6d2fb2
Chermaine Zimmerman <C14jcdj@yahoo.com>
2014-05-21 20:08:50 -0700

    adds appointment side bar

--- calender.js
+++ calender.js
@@ -21,8 +21,8 @@ Calender.prototype = {
 
     getCalenderInfo: function() {
         //Would have ajax call to server to get JSON
-        //returning Month and Day the first is on
-        return ["May", "Thursday", 30]
+        //returning Month, Day of first of the month, number of days in month
+        return ["May", "Thursday", 31]
     },
 
     fillOutCalender: function(info) {
@@ -41,6 +41,75 @@ Calender.prototype = {
             }
         }
 
+    },
+
+    bindDays: function() {
+        var that = this;
+        console.log(that)
+        $('td').on('click', function() {
+            var day = $(this).html();
+            var month = $('th').html();
+            var data = {
+                'day': day,
+                'month': month
+            }
+
+            //Send ajax request for this month and day appointments
+            var ajaxresponse = [{
+                "event_name": "Event A",
+                "start_time": 9,
+                "end_time": 10
+            }, {
+                "event_name": "Event B",
+                "start_time": 11,
+                "end_time": 12
+            }, {
+                "event_name": "Event C",
+                "start_time": 11.5,
+                "end_time": 12.5
+            }, {
+                "event_name": "Event D",
+                "start_time": 13,
+                "end_time": 14.5
+            }, {
+                "event_name": "Event E",
+                "start_time": 13,
+                "end_time": 13.5
+            }, {
+                "event_name": "Event F",
+                "start_time": 14,
+                "end_time": 14.5
+            }, {
+                "event_name": "Event G",
+                "start_time": 15,
+                "end_time": 16
+            }, {
+                "event_name": "Event H",
+                "start_time": 15,
+                "end_time": 16.5
+            }, {
+                "event_name": "Event I",
+                "start_time": 15.5,
+                "end_time": 17.0
+            }, {
+                "event_name": "Event J",
+                "start_time": 16.5,
+                "end_time": 17.5
+            }]
+            that.displayAppointments(ajaxresponse)
+        })
+    },
+
+    displayAppointments: function(appointments) {
+        console.log(appointments)
+        $('table').css({
+            'width': '60%',
+            'height': '400px',
+            'margin-left': '5%',
+            'float': 'left',
+            'position': 'relative'
+        })
+        $('.day').css('display', 'block')
     }
 
 
@@ -51,7 +120,9 @@ function CalenderController() {}
 CalenderController.prototype = {
 
     run: function(calender) {
-        info = calender.getCalenderInfo();
+        var info = calender.getCalenderInfo();
         calender.fillOutCalender(info);
+        calender.bindDays();
+
     }
 }
